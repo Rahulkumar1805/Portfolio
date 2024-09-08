09cc00672ecba94f6956e0ecb6a7af0db7b0a46c
rahulkumar1805 <rahulfrenzi70@gmail.com>
2024-09-09 03:12:58 +0530

    till contact emai

---
+++ assets/js/app.js
@@ -0,0 +1,289 @@
+
+particlesJS('particles-js', {
+  particles: {
+    number: {
+      value: 140,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: ['#00675e', '#dde165', '#E01E5B', '#36C5F0'],
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#fff',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: 'https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png',
+        width: 100,
+        height: 100,
+      },
+    },
+
+    opacity: {
+      value: 0.8,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 5,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 2,
+        size_min: 10,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#808080',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      onclick: {
+        enable: false,
+        mode: 'push',
+      },
+    },
+    modes: {
+      repulse: {
+        distance: 70,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+    },
+  },
+  retina_detect: true,
+})
+
+/* ---- particles.js config ---- */
+
+particlesJS('particlest-js', {
+  particles: {
+    number: {
+      value: 140,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: ['#00675e', '#dde165', '#E01E5B', '#36C5F0'],
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#fff',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: 'https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png',
+        width: 100,
+        height: 100,
+      },
+    },
+
+    opacity: {
+      value: 0.8,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 5,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 10,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#808080',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      onclick: {
+        enable: false,
+        mode: 'push',
+      },
+    },
+    modes: {
+      repulse: {
+        distance: 70,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+    },
+  },
+  retina_detect: true,
+})
+
+/* ---- particles.js config ---- */
+
+particlesJS('particlesm-js', {
+  particles: {
+    number: {
+      value: 140,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: ['#00675e', '#dde165', '#E01E5B', '#36C5F0'],
+    },
+    shape: {
+      type: 'circle',
+      stroke: {
+        width: 0,
+        color: '#fff',
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: 'https://cdn.freebiesupply.com/logos/large/2x/slack-logo-icon.png',
+        width: 100,
+        height: 100,
+      },
+    },
+
+    opacity: {
+      value: 0.8,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 5,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 8,
+      random: true,
+      anim: {
+        enable: false,
+        speed: 10,
+        size_min: 10,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#808080',
+      opacity: 0.4,
+      width: 1,
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: 'none',
+      random: false,
+      straight: false,
+      out_mode: 'out',
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: 'window',
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+      onclick: {
+        enable: false,
+        mode: 'push',
+      },
+    },
+    modes: {
+      repulse: {
+        distance: 70,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+    },
+  },
+  retina_detect: true,
+})
